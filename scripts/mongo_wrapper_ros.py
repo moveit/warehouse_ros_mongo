3a8fc7149970af079e2b91557f3bdcf13236ec15
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-07-21 07:05:48 +0200

    Handle permission errors creating database path
    Try default_db_path instead.

--- scripts/mongo_wrapper_ros.py
+++ scripts/mongo_wrapper_ros.py
@@ -7,6 +7,10 @@ import sys
 import os
 import shutil
 
+
+default_db_path = '/tmp/db'
+
+
 def print_help_message():
     print("""
 Usage: rosrun warehouse_ros_mongo mongo_wrapper_ros.py
@@ -17,9 +21,10 @@ Start the mongodb database server, configured using the following ROS parameters
   - warehouse_host: doesn't directly affect server, but used by clients to know where the db is.
 
 * parameters in parent namespace
-  - db_path: where the db is stored on the filesystem.  Defaults to /tmp/db.
+  - db_path: where the db is stored on the filesystem.  Defaults to {0}.
   - overwrite: whether to overwrite existing database if it exists.  Defaults to false.
-""")
+""".format(default_db_path))
+
 
 if '--help' in sys.argv:
     print_help_message()
@@ -28,7 +33,7 @@ if '--help' in sys.argv:
 rospy.init_node('mongodb')
 path_param = rospy.get_param('~database_path' , None)
 if path_param is None:
-    path_param = rospy.get_param('~db_path' , '/tmp/db')
+    path_param = rospy.get_param('~db_path' , default_db_path)
 else:
     print('Parameter "database_path" is deprecated. Please use "db_path" instead.')
 dbpath = os.path.expanduser(path_param)
@@ -40,7 +45,7 @@ if '--repair' in sys.argv:
     if os.path.exists(lock_file):
         rospy.loginfo("  Removing lock file")
         os.remove(lock_file)
-    check_call(['mongodb', 'mongod', '--repair', '--dbpath', dbpath.format(dbpath)])
+    check_call(['mongodb', 'mongod', '--repair', '--dbpath', dbpath])
     rospy.loginfo("  Successfully repaired.")
     sys.exit(0)
 
@@ -49,12 +54,21 @@ port = rospy.get_param('warehouse_port', 27017)
 host = rospy.get_param('warehouse_host', 'localhost')
 
 if overwrite and os.path.exists(dbpath):
-    shutil.rmtree(dbpath)
     rospy.loginfo('Removed existing db at %s', dbpath)
+    shutil.rmtree(dbpath)
 
-if not os.path.exists(dbpath):
-    rospy.loginfo('{0} did not exist; creating it.'.format(dbpath))
-    os.makedirs(dbpath)
+while not os.path.exists(dbpath):
+    rospy.loginfo('{0} does not exist; creating it.'.format(dbpath))
+    try:
+        os.makedirs(dbpath)
+        break
+    except OSError as e:
+        if dbpath != default_db_path:
+            rospy.logerr('{0}. Trying {1} instead.'.format(str(e), default_db_path))
+            dbpath = default_db_path
+        else:
+            rospy.logfatal(e)
+            raise e
 
 rospy.loginfo('Starting mongodb with db location {0} listening on {2}:{1}'.\
               format(dbpath, port, host))
