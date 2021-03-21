5138ab046d2b36ed226890c36aa5be5c8d57b1cd
Tyler Weaver <tylerjw@gmail.com>
2021-03-21 16:09:41 -0600

    Fix black formatting errors

--- test/warehouse_ros_mongo.launch.py
+++ test/warehouse_ros_mongo.launch.py
@@ -2,18 +2,22 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_testing.actions import ReadyToTest
 
-def generate_test_description():
-    return LaunchDescription([
-        Node(
-            package='warehouse_ros_mongo',
-            executable='mongo_wrapper_ros.py',
-            parameters=[
-                {'warehouse_port': 33829},
-                {'warehouse_host': 'localhost'},
-                {'warehouse_plugin': 'warehouse_ros_mongo::MongoDatabaseConnection'}
-            ],
-            output='screen'
-        ),
 
-        ReadyToTest()
-    ])
+def generate_test_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="warehouse_ros_mongo",
+                executable="mongo_wrapper_ros.py",
+                parameters=[
+                    {"warehouse_port": 33829},
+                    {"warehouse_host": "localhost"},
+                    {
+                        "warehouse_plugin": "warehouse_ros_mongo::MongoDatabaseConnection"
+                    },
+                ],
+                output="screen",
+            ),
+            ReadyToTest(),
+        ]
+    )
